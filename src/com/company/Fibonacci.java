aa2161a089facb52132a191d9e0ea560b1509412
antoniocassano84 <antonio.cassano84@gmail.com>
2019-11-21 04:58:01 +0000

    added more info

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -3,10 +3,8 @@ package com.company;
 class Fibonacci {
 
     static long fibonacci_recursive(long n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+        if (n <= 1) {
+            return n;
         } else {
             return fibonacci_recursive(n-1) + fibonacci_recursive(n-2);
         }
