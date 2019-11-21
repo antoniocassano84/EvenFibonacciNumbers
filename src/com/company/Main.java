b57491fc0b26e0e44e3b576220bb04c957d12271
antoniocassano84 <antonio.cassano84@gmail.com>
2019-11-21 04:53:42 +0000

    added more info

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,7 @@ package com.company;
 
 public class Main {
 
-    private static long fibonacci_recursive(long n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return fibonacci_recursive(n-1) + fibonacci_recursive(n-2);
-        }
-    }
+
     public static void main(String[] args) {
             int i = 1;
             int sum = 0;
@@ -18,14 +10,16 @@ public class Main {
 
             long preTime=System.currentTimeMillis();
             do {
-                nextFib = fibonacci_recursive(i);
+                nextFib = Fibonacci.fibonacci_recursive(i);
                 if(nextFib % 2 == 0){
                     sum += nextFib;
                 }
                 i++;
             } while(nextFib < 4_000_000);
             long postTime=System.currentTimeMillis();
+            
             System.out.println("Sum is " + sum);
-            System.out.println("Computation Time (milliseconds) for fibonacci_recursive: "+(postTime-preTime));
+            System.out.println("Computation Time (milliseconds) for fibonacci_recursive: " +
+                    (postTime-preTime));
         }
     }
