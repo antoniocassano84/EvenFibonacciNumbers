6ada4f108be6a0cdcbd186efbe2eb7615caedef0
antoniocassano84 <antonio.cassano84@gmail.com>
2019-11-21 06:45:20 +0000

    test class added

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -1,8 +1,8 @@
 package com.company;
 
-class Fibonacci {
+public class Fibonacci {
 
-    static long fibonacci_recursive(long n) {
+    public static long fibonacci_recursive(long n) {
         if (n <= 1) {
             return n;
         } else {
--- src/test/FibonacciTest.java
+++ src/test/FibonacciTest.java
@@ -2,16 +2,28 @@ package test;
 
 import com.company.Fibonacci;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 
 class FibonacciTest {
 
     @Test
-    void testFibonacciRecursiveFirstTwoValues() {
-        int[][] testCases = { { 0, 0 }, { 1, 1 } };
+    void testFibonacciRecursiveFirstFourValues() {
+        int[][] testCases = { { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 } };
         for(int[] testCase : testCases) {
             Assert.assertEquals(testCase[1], Fibonacci.fibonacci_recursive(testCase[0]));
         }
+    }
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    public void testIllegalArgumentException() throws IllegalArgumentException {
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage("Negative numbers not valid");
+        Fibonacci.fibonacci_recursive(-1);
     }
 }
